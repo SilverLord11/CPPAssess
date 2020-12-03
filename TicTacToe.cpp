33489e2f1d5ca68aa5c1376babe7907e10951d4f
Logan Holt <WolfGuardLegion11@gmail.com>
2020-12-03 12:36:18 -0800

    Tic Tac Toe Transferred, but Numbers game and Battle arena lost. Must've accidentally deleted them thinking  they were different files. This is what I get for not naming them properly.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,26 +15,26 @@ void display_board() {
 
     //Rander Game Board LAYOUT
 
-    cout << "PLAYER - 1 [X]t PLAYER - 2 [O]nn";
-    cout << "tt     |     |     n";
-    cout << "tt  " << board[0][0] << "  | " << board[0][1] << "  |  " << board[0][2] << " n";
-    cout << "tt_____|_____|_____n";
-    cout << "tt     |     |     n";
-    cout << "tt  " << board[1][0] << "  | " << board[1][1] << "  |  " << board[1][2] << " n";
-    cout << "tt_____|_____|_____n";
-    cout << "tt     |     |     n";
-    cout << "tt  " << board[2][0] << "  | " << board[2][1] << "  |  " << board[2][2] << " n";
-    cout << "tt     |     |     n";
+    cout << "PLAYER - 1 [X]t PLAYER - 2 [O]\n \n";
+    cout << "\t\t     |     |     \n";
+    cout << "\t\t  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << " \n";
+    cout << "\t\t_____|_____|_____\n";
+    cout << "\t\t     |     |     \n";
+    cout << "\t\t  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << " \n";
+    cout << "\t\t_____|_____|_____\n";
+    cout << "\t\t     |     |     \n";
+    cout << "\t\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << " \n";
+    cout << "\t\t     |     |     \n";
 }
 
 //Function to get the player input and update the board
 
 void player_turn() {
     if (turn == 'X') {
-        cout << "ntPlayer - 1 [X] turn : ";
+        cout << "\n\tPlayer - 1 [X] turn : ";
     }
     else if (turn == 'O') {
-        cout << "ntPlayer - 2 [O] turn : ";
+        cout << "\n\tPlayer - 2 [O] turn : ";
     }
     //Taking input from user
     //updating the board according to choice and reassigning the turn Start
@@ -44,32 +44,62 @@ void player_turn() {
     //switch case to get which row and column will be update
 
     switch (choice) {
-    case 1: row = 0; column = 0; break;
-    case 2: row = 0; column = 1; break;
-    case 3: row = 0; column = 2; break;
-    case 4: row = 1; column = 0; break;
-    case 5: row = 1; column = 1; break;
-    case 6: row = 1; column = 2; break;
-    case 7: row = 2; column = 0; break;
-    case 8: row = 2; column = 1; break;
-    case 9: row = 2; column = 2; break;
+    case 1: 
+        row = 0; column = 0; 
+        break;
+
+    case 2: 
+        row = 0; column = 1; 
+        break;
+
+    case 3: 
+        row = 0; column = 2; 
+        break;
+
+    case 4: 
+        row = 1; column = 0; 
+        break;
+
+    case 5: 
+        row = 1; column = 1; 
+        break;
+
+    case 6: 
+        row = 1; column = 2; 
+        break;
+
+    case 7: 
+        row = 2; column = 0; 
+        break;
+
+    case 8: 
+        row = 2; column = 1; 
+        break;
+
+    case 9: 
+        row = 2; column = 2; 
+        break;
+
     default:
         cout << "Invalid Move";
     }
 
-    if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O') {
+    if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O') 
+    {
         //updating the position for 'X' symbol if
         //it is not already occupied
         board[row][column] = 'X';
         turn = 'O';
     }
-    else if (turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O') {
+    else if (turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O') 
+    {
         //updating the position for 'O' symbol if
         //it is not already occupied
         board[row][column] = 'O';
         turn = 'X';
     }
-    else {
+    else 
+    {
         //if input position already filled
         cout << "Box already filled!n Please choose another!!nn";
         player_turn();
@@ -106,19 +136,19 @@ bool gameover() {
 
 int main()
 {
-    cout << "tttT I C K -- T A C -- T O E -- G A M Ettt";
-    cout << "nttttFOR 2 PLAYERSnttt";
+    cout << "\t\t\tT I C K -- T A C -- T O E -- G A M E\t\t\t";
+    cout << "\n\t\t\t\tFOR 2 PLAYERS\n\t\t\t";
     while (gameover()) {
         display_board();
         player_turn();
         gameover();
     }
     if (turn == 'X' && draw == false) {
-        cout << "nnCongratulations!Player with 'X' has won the game";
+        cout << "\n\nCongratulations! The 2nd Player has won the game";
     }
     else if (turn == 'O' && draw == false) {
-        cout << "nnCongratulations!Player with 'O' has won the game";
+        cout << "\n\nCongratulations! The 1st Player has won the game";
     }
     else
-        cout << "nnGAME DRAW!!!nn";
+        cout << "\n\nGAME DRAW!!!\n\n";
 }
