d82fde971b877e1a1c1f6fb586fdc1f091a7965f
Logan Holt <WolfGuardLegion11@gmail.com>
2020-12-06 17:32:59 -0800

    Commentary for numbers game

--- Number Guessing game.cpp
+++ Number Guessing game.cpp
@@ -3,12 +3,15 @@
 
 void guessingGame()
 {
+	//variable declaration
 	bool GameOver = false;
 	std::cout << "Welcome To the numbers game. \n";
 	int min = 0;
 	int max = 1000;
 	int guess = (min + max)/2;
 	int choice;
+
+	//These are the controls for player input
 	std::cout << "I will try to guess the correct answer from 1 to 1000 \n";
 	std::cout << "The controls are '1' for higher, '2' for lower, and '3' for the answer \n";
 
@@ -16,7 +19,10 @@ void guessingGame()
 	{
 		std::cout << "Is your number " << guess << "?\n";
 		
+		//takes the player input
 		std::cin >> choice;
+
+		//switch case to get how the commands update the game
 		switch (choice)
 		{
 			case 1:
